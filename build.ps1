2fccdf540ef0dd6a83a31011ec6cbbb2d4f3dac5
Florian Schäffeler <florian.schaeffeler+github@gmail.com>
2022-03-31 09:01:50 +0200

    last changes to the example sources

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@
 Remove-Item ./build -Recurse -Force -ErrorAction Ignore
 
 cmake -B build/ -S .
+#cmake -B build/ -S . -DBUILD_TESTS=ON
 if( !$? ) {
     Write-Error "Cmake configure failed"
     Exit 1
